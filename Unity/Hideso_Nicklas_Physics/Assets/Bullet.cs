e361740eb460031ecd955e46972a924046a80dc2
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-10-24 11:22:00 +0200

    WIP

---
+++ Unity/Hideso_Nicklas_Physics/Assets/Bullet.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] AudioClip explosionSound = null;
+    SpriteRenderer sprite;
+    AudioSource audioPlayer;
+
+    bool destroying = false;
+
+    public float speed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (destroying) { return; }
+        float xPos = transform.position.x + speed * Time.deltaTime;
+        transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
+    }
+
+    public void setBulletSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!destroying) 
+        {
+            StartCoroutine(DestroyBullet());
+            destroying = true;
+        }
+  
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        audioPlayer.PlayOneShot(explosionSound);
+        yield return new WaitForSeconds(explosionSound.length);
+        Destroy(gameObject);
+    }
+}
---
+++ Unity/Hideso_Nicklas_Physics/Assets/Gun.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using TMPro;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] Bullet bullet = null;
+    [SerializeField] float shootinVelocity = 10f;
+    [SerializeField] AudioClip shotSound = null;
+    [SerializeField] float fireDelay = 1f;
+
+    AudioSource audioPlayer;
+
+    bool firing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Fire1") != 0)
+        {
+            if(!firing)
+            {
+                firing = true;
+                StartCoroutine(Fire());
+            }
+        }
+    }
+
+    IEnumerator Fire()
+    {
+        bullet.setBulletSpeed(shootinVelocity);
+        Bullet newBullet = bullet;
+        Instantiate(newBullet, transform.position, Quaternion.identity);
+        audioPlayer.PlayOneShot(shotSound);
+        yield return new WaitForSeconds(fireDelay);
+        firing = false;
+    }
+}
